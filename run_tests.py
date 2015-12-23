9ef537d02b2f50bf5894f0ec54e5df7d22ec083c
Konstantin Vlasov <kvlasov@odin.com>
2015-12-23 20:11:30 +0300

    Fixed constructing package bundles for tests
    * Instead of bundling RPM packages by their architecture, bundle them by the
      build task.
    * When testing, use repository with architecture specified in the build task.
    * For noarch build task, perform two separate tests with repositories for both
      architectures.

--- run_tests.py
+++ run_tests.py
@@ -24,32 +24,41 @@ class RunTestsTask(BaseTaskHandler):
 
     # The task handler
     def handler(self, tag_id, build_id):
-        rpms = self.session.listBuildRPMs(build_id)
+        # Retrieve all necessary information
         tag_info = self.session.getTag(tag_id, strict=True)
-        #build_info = self.session.getBuild(build_id)
+        build_info = self.session.getBuild(build_id)
+        subtasks = self.session.getTaskChildren(build_info['task_id'])
 
-        # List all available RPM files
-        rpm_files = {}
-        for rpm_info in rpms:
-            if rpm_info['arch'] == 'src':
-                # Skip src.rpm
+        # For each build task, get the list of built RPM files
+        # and try to test-install them using the current build tag repos.
+        for buildTask in subtasks:
+            if buildTask['method'] != 'buildArch':
+                # Skip everything except actual build tasks
                 continue
-            rpm_fname = "%s.%s.rpm" % (rpm_info['nvr'], rpm_info['arch'])
-            rpm_path = "/mnt/koji/packages/" + "/".join(map(rpm_info.get, ['name', 'version', 'release', 'arch']) + [rpm_fname])
-            rpm_files.setdefault(rpm_info['arch'], []).append(rpm_path)
 
-        # Test the packages from each architecture
-        for arch in rpm_files.keys():
-            # Construct path to the build tag repository
-            repo_path = "/mnt/koji/repos/" + tag_info['name'] + "-build/latest/"
-            if arch == "i686":
-                # Hack for different arch names in packages and repos
-                repo_path += "i386/"
+            # Configure repositories to test packages with:
+            # 1. for arch-specific build, using the corresponding repository;
+            # 2. for noarch build, testing with both repositories.
+            archs = []
+            if buildTask['arch'] == 'x86_64':
+                archs = ['x86_64']
+            elif buildTask['arch'] == 'i686':
+                # Fix arch name for repository
+                archs = ['i386']
+            elif buildTask['arch'] == 'noarch':
+                archs = ['i386', 'x86_64']
             else:
-                repo_path += arch + "/"
-            # Yum config for installation test
-            (yumcfg_fd, yumcfg_file) = mkstemp(prefix='koji-test-yum-', suffix='.cfg', text=True)
-            os.write(yumcfg_fd, """
+                raise koji.PostBuildError, "Unsupported build architecture: %s" % (buildTask['arch'])
+
+            taskResult = self.session.getTaskResult(buildTask['id'])
+            rpms = [('/mnt/koji/work/' + rpm) for rpm in taskResult['rpms']]
+
+            for arch in archs:
+                # Construct path to the build tag repository
+                repo_path = "/mnt/koji/repos/" + tag_info['name'] + "-build/latest/" + arch + "/"
+                # Yum config for installation test
+                (yumcfg_fd, yumcfg_file) = mkstemp(prefix='koji-test-yum-', suffix='.cfg', text=True)
+                os.write(yumcfg_fd, """
 [main]
 cachedir=/var/cache/yum
 debuglevel=1
@@ -66,22 +75,22 @@ baseurl=file://%s
 enabled=1
 gpgcheck=0
 """ % (repo_path))
-            os.close(yumcfg_fd)
+                os.close(yumcfg_fd)
 
-            # Root directory for installation test
-            tmp_dir = mkdtemp(prefix='koji-test-root-')
+                # Root directory for installation test
+                tmp_dir = mkdtemp(prefix='koji-test-root-')
 
-            log_fname = "tests-" + arch + ".log"
-            log_fpath = tmp_dir + "/" + log_fname
-            cmdline = "yum install -v -y --config=%(yumcfg_file)s --installroot=%(tmp_dir)s " % locals() + " ".join(rpm_files[arch])
-            res = self.execLog(cmdline, log_fpath)
-            self.uploadFile(log_fpath)
-            shutil.rmtree(tmp_dir)
-            os.unlink(yumcfg_file)
+                log_fname = "tests-b" + str(buildTask['id']) + '-' + arch + ".log"
+                log_fpath = tmp_dir + "/" + log_fname
+                cmdline = "yum install -v -y --config=%(yumcfg_file)s --installroot=%(tmp_dir)s " % locals() + " ".join(rpms)
+                res = self.execLog(cmdline, log_fpath)
+                self.uploadFile(log_fpath)
+                shutil.rmtree(tmp_dir)
+                os.unlink(yumcfg_file)
 
-            # Check result
-            if res != 0:
-                raise koji.PostBuildError, "Installation test failed, see %s for details." % (log_fname)
+                # Check result
+                if res != 0:
+                    raise koji.PostBuildError, "Installation test failed, see %s for details." % (log_fname)
 
         return "Result: success"
 
